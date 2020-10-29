f98254a10a76abe1fa55c0ce24f698a05aa1738f
Lud-ly <Ludom82@gmail.com>
2020-10-30 00:13:17 +0100

    footer & css

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,9 +4,9 @@ class Footer extends Component {
     render() {
         return (
             <div>
-                <div className="footer">
+                <div className="col-md-12 footer">
                     <a href={"ludovic-mouly.com"}><p>@ludovic</p></a>
-                   <p>&copy copyright 2020</p>
+                   <p>© 2020</p>
                 </div>
             </div>
         )
