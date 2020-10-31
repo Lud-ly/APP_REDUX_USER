722fa89eaca9c15850ca8a0a093b4a00d2600149
Lud-ly <Ludom82@gmail.com>
2020-10-31 18:51:30 +0100

    img json & card

--- src/containers/user_detail.js
+++ src/containers/user_detail.js
@@ -10,7 +10,7 @@ class UserDetail extends Component {
                 <div>
                     <div className="col-md-9 ready">
                         <h1>Selectionnez pour démarrer</h1>
-                        <img src={"/images/linux_011.jpg"}/>
+                        <img src={"/images/linux_011.jpg"} />
                     </div>
                 </div>
             )
@@ -18,29 +18,33 @@ class UserDetail extends Component {
         return (
             <div>
                 <div className="col-md-9">
-                <img src={myActiveUser.img}/>
-                    <h3>STATISTIQUES JOUEURS : <br /><br />  U11 de {myActiveUser.club}<br /><br /> Voici {myActiveUser.name}<br /></h3>
-                    <ul>
-                       
-                        <li>Numéro : {myActiveUser.id}</li><br />
-                        <li>Poste : {myActiveUser.role}</li><br />
-                        <li>Titulaire : {myActiveUser.active}</li><br />
-                        <li>pied : {myActiveUser.pied}</li><br />
-                        <li>Nombres de buts : {myActiveUser.but}</li><br />
-                        <li>point fort : {myActiveUser.power}</li><br />
-                        <li>manager : {myActiveUser.manager}</li><br />
-                        <li>hired_on : {myActiveUser.hired_on}</li><br />
-                        <li>salary : {myActiveUser.salary}</li><br />
-                        <li>commission : {myActiveUser.commission}</li><br />
-                        <li>designation : {myActiveUser.designation}</li><br />
-                        <li>dept : {myActiveUser.dept}</li><br />
-                        <li>Année : {myActiveUser.year}</li><br />
-                        <li>Marque : {myActiveUser.make}</li><br />
-                        <li>Modèle : {myActiveUser.model}</li><br />
-                        <li>Coupe : {myActiveUser.body_styles}</li><br />
-                    </ul>
-                    <h2 style={{ "marginTop": "50px" }}>SAISON 2020-2021{myActiveUser.college}</h2>
-                    <h2>SAISON 2020-2021{myActiveUser.year}</h2>
+                    <div className="card">
+                        <div className="card-header">
+                            <h3>TOP 10 MEILLEURS ACTEURS{myActiveUser.club} Voici {myActiveUser.name}</h3>
+                        </div>
+                        <div className="card-body">
+                            <img src={myActiveUser.img} />
+                            <ul>
+                                <li>Numéro : {myActiveUser.id}</li><br />
+                                <li>Poste : {myActiveUser.role}</li><br />
+                                <li>Titulaire : {myActiveUser.active}</li><br />
+                                <li>pied : {myActiveUser.pied}</li><br />
+                                <li>Nombres de buts : {myActiveUser.but}</li><br />
+                                <li>point fort : {myActiveUser.power}</li><br />
+                                <li>manager : {myActiveUser.manager}</li><br />
+                                <li>hired_on : {myActiveUser.hired_on}</li><br />
+                                <li>salary : {myActiveUser.salary}</li><br />
+                                <li>commission : {myActiveUser.commission}</li><br />
+                                <li>designation : {myActiveUser.designation}</li><br />
+                                <li>dept : {myActiveUser.dept}</li><br />
+                                <li>Année : {myActiveUser.year}</li><br />
+                                <li>Marque : {myActiveUser.make}</li><br />
+                                <li>Modèle : {myActiveUser.model}</li><br />
+                                <li>Coupe : {myActiveUser.body_styles}</li><br />
+                            </ul>
+                        </div>
+                    </div>
+                    <h2>SAISON 2020-2021{myActiveUser.year}{myActiveUser.college}</h2>
                 </div>
             </div>
         )
