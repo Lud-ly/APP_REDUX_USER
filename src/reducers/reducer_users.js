0dc29f543b6d45502306c343873c16c85a83e1f4
Lud-ly <Ludom82@gmail.com>
2020-11-01 19:39:57 +0100

    rewrite Json with img

--- src/reducers/reducer_users.js
+++ src/reducers/reducer_users.js
@@ -3,286 +3,84 @@
 export default function () {
   return [
     {
-      name: 'THIBAULT MOULY',
+      name: 'AL PACINO',
       id: 1,
-      role: 'AILIER-ATTAQUANT',
-      active: 'OUI',
-      pied: 'gauche',
-      but: 12,
-      power: 'GROSSE FRAPPE',
-      college: 'sport-etude',
-      club: 'OM',
-      img: "../images/act7.jpg"
+      films: 'ESCM34',
+      img: "../images/act7.jpg",
+      imgThumbnail: "../images/act7Thumbnail.jpg"
     },
     {
-      name: 'MAEL PYTHON',
-      id: 2, role: 'AILIER-MENEUR-JEU',
-      active: 'OUI',
-      pied: 'droit',
-      but: 11,
-      power: 'DRIBLE',
-      college: 'au collège',
-      club: 'ESCM34',
-      img: "../images/act5.jpg"
+      name: 'BRUCE WILLIS',
+      id: 2,
+      films: 'ESCM34',
+      img: "../images/act5.jpg",
+      imgThumbnail: "../images/act5Thumbnail.jpg"
     },
     {
-      name: 'RAPH JUMEAU',
+      name: 'MATT DAMON',
       id: 3,
-      role: 'MILIEU-DEFENSIF',
-      active: 'NON',
-      pied: 'droit',
-      but: 3,
-      power: 'DRIBLE',
-      college: 'au collège',
-      club: 'ESCM34',
-      img: "../images/act3.jpg"
+      films: 'ESCM34',
+      img: "../images/act3.jpg",
+      imgThumbnail: "../images/act3Thumbnail.jpg"
     },
     {
-      name: 'YOURSI WITSEL',
+      name: 'BRAD PITT',
       id: 4,
-      role: 'MILIEU-OFFENSIF',
-      active: 'NON',
-      pied: 'gauche',
-      but: 5,
-      power: 'DRIBLE',
-      college: 'sport-etude',
-      club: 'MMF',
-      "img": "../images/act6.jpg"
+      img: "../images/act6.jpg",
+      imgThumbnail: "../images/act6Thumbnail.jpg"
     },
     {
-      name: 'LOUIS DIT LOULOU',
+      name: 'SYLVESTER STALLONE',
       id: 5,
-      role: 'AILIER-ATTAQUANT',
-      active: 'OUI',
-      pied: 'gauche',
-      but: 12,
-      power: 'GROSSE FRAPPE',
-      college: 'au collège',
-      club: 'MHSC',
-      img: "../images/act2.jpg"
+      films: 'MHSC',
+      img: "../images/act2.jpg",
+      imgThumbnail: "../images/act2Thumbnail.jpg"
     },
     {
-      name: 'SACHA GOMAR',
+      name: 'CLINT EASTWOOD',
       id: 6,
-      role: 'MILIEU-DEFENSIF',
-      active: 'OUI',
-      pied: 'droit',
-      but: 11,
-      power: 'DRIBLE',
-      college: 'sport-etude',
-      club: 'AS LATTES',
-      img: "../images/act4.jpg"
+      films: "Un frisson dans la nuit (1971) · L'Homme des Hautes Plaines (1973) · Breezy (1973) · La Sanction (1975) · Josey Wales hors-la-loi (1976) · L'Épreuve de force (1977) · Bronco Billy (1980) · Firefox, l'arme absolue (1982) · Honkytonk Man (1982) · Le Retour de l'inspecteur Harry (1983) · Pale Rider, le cavalier solitaire (1985) · Le Maître de guerre (1986) · Bird (1988) · Chasseur blanc, cœur noir (1990) · La Relève (1990) · Impitoyable (1992) · Un monde parfait (1993) · Sur la route de Madison (1995) · Les Pleins Pouvoirs (1997) · Minuit dans le jardin du bien et du mal (1998) · Jugé coupable (1999) · Space Cowboys (2000) · Créance de sang (2002) · The Blues : Piano Blues (2003) · Mystic River (2003) · Million Dollar Baby (2004) · Mémoires de nos pères (2006) · Lettres d'Iwo Jima (2007) · Article de qualité L'Échange (2008) · Gran Torino (2008) · Invictus (2009) · Au-delà (2010) · J. Edgar (2011) · Jersey Boys (2014) · American Sniper (2015) · Sully (2016) · Le 15 h 17 pour Paris (2018) · La Mule (2018) · Le Cas Richard Jewell (2019)",
+      img: "../images/act4.jpg",
+      imgThumbnail: "../images/act4Thumbnail.jpg"
     },
     {
-      name: 'ENZO PALAISI',
+      name: 'ROBERT DE NIRO',
       id: 7,
-      role: 'GARDIEN',
-      active: 'NON',
-      pied: 'droit',
-      but: 3,
-      power: 'DRIBLE',
-      college: 'au collège',
-      club: 'SETE',
-      img: "../images/act8.jpg"
+      films: 'SETE',
+      img: "../images/act8.jpg",
+      imgThumbnail: "../images/act8Thumbnail.jpg"
     },
     {
-      name: 'LEO JIMMY',
+      name: 'LEONARDO DI CAPRIO',
       id: 8,
-      role: 'MILIEU-DEFENSIF',
-      active: 'NON',
-      pied: 'gauche',
-      but: 5,
-      power: 'DRIBLE',
-      college: 'au collège',
-      club: 'ESCM34',
-      img: "../images/act10.jpg"
+      films: "jhfsk",
+      img: "../images/act10.jpg",
+      imgThumbnail: "../images/act10Thumbnail.jpg"
     },
     {
-      name: 'ENZO FRANGON',
+      name: 'JOHNNY DEPP',
       id: 9,
-      role: 'DEFENSEUR',
-      active: 'NON',
-      pied: 'droit',
-      but: 3,
-      power: 'DRIBLE',
-      college: 'sport-etude',
-      club: 'BEAUCAIRE',
-      img: "../images/act1.jpg"
+      films: 'BEAUCAIRE',
+      img: "../images/act1.jpg",
+      imgThumbnail: "../images/act1Thumbnail.jpg"
     },
 
     {
-      name: 'PIERRE JUMEAU',
+      name: 'SAMUEL L JACKSON',
       id: 10,
-      role: 'MILIEU-DEFENSIF',
-      active: 'NON',
-      pied: 'gauche',
-      but: 5,
-      power: 'DRIBLE',
-      college: 'sport-etude',
-      club: 'OM',
-      img: "../images/act9.jpg"
-    },
-
-    {
-      "id": 11,
-      "name": "GARFIELD MATOU",
-      "designation": "MANAGER",
-      "manager": 9,
-      "hired_on": "05-01-1993",
-      "salary": 54000,
-      "commission": "fake",
-      "dept": 4,
-      "img": "../images/167.jpg"
+      films: 'OM',
+      img: "../images/act9.jpg",
+      imgThumbnail: "../images/act9Thumbnail.jpg"
     },
     {
-      "id": 12,
-      "name": "GRANT PA JOU",
-      "designation": "ENGINEER IA",
-      "manager": 10,
-      "hired_on": "03-30-1997",
-      "salary": 32000,
-      "commission": null,
-      "dept": 2,
-      "img": "../images/act1.jpg"
-    },
-    {
-      "id": 13,
-      "name": "CHARLY HARDING",
-      "designation": "MANAGER",
-      "manager": 9,
-      "hired_on": "02-02-1998",
-      "salary": 52000,
-      "commission": 300,
-      "dept": 3,
-      "img": "../images/act2.jpg"
-    },
-    {
-      "id": 14,
-      "name": "HOOVER FREEZE",
-      "designation": "SALES I",
-      "manager": 2,
-      "hired_on": "04-02-1990",
-      "salary": 27000,
-      "commission": "BIGDATA",
-      "dept": 3,
-      "img": "../images/act3.jpg"
-    },
-    {
-      "id": 15,
-      "name": "JACKSON FIVE",
-      "designation": "CEO",
-      "manager": "DEVELOPER",
-      "hired_on": "01-01-1990",
-      "salary": 75000,
-      "commission": "commission",
-      "dept": 4,
-      "img": "../images/act4.jpg"
-    },
-    {
-      "id": 16,
-      "name": "JOHNSON",
-      "designation": "ADMIN",
-      "manager": 6,
-      "hired_on": "12-17-1990",
-      "salary": 18000,
-      "commission": "ADMIN",
-      "dept": 4,
-      "img": "../images/act5.jpg"
-    },
-    {
-      "id": 17,
-      "name": "LINCOLN HAWK",
-      "designation": "I eTECH",
-      "manager": 6,
-      "hired_on": "06-23-1994",
-      "salary": 29500,
-      "commission": " 300 files",
-      "dept": 4,
-      "img": "../images/act6.jpg"
-    },
-    {
-      "id": 18,
-      "name": "ENRIQUE POLK",
-      "designation": "TECH INTELLI-j",
-      "manager": 6,
-      "hired_on": "09-22-1997",
-      "salary": 45000,
-      "commission": "3TERA",
-      "dept": 4,
-      "img": "../images/act 7.jpg"
-    },
-    {
-      "id": 19,
-      "name": "CHRISTOPHER TAFT",
-      "designation": "SALES I",
-      "manager": 2,
-      "hired_on": "01-02-1996",
-      "salary": 65000,
-      "commission": "500virus",
-      "dept": 3,
-      "img": "../images/act8.jpg"
-    },
-   
-  {
-    "id" : 20,
-      "year": 2004,
-      "make": "Acura",
-      "model": "NSX",
-      "body_styles": "[\"Coupe\"]",
-      "name": "SYLVESTER STALLONE",
-      "designation": "SALES I",
-      "manager": 2,
-      "hired_on": "01-02-1996",
-      "salary": 65000,
-      "commission": "500virus",
-      "dept": 3,
-      "img": "../images/act9.jpg"
-    },
-    {
-      "id" : 21,
-      "year": 2004,
-      "make": "Acura",
-      "model": "RL",
-      "body_styles": "[\"Sedan\"]",
-      "name": "SAMUEL L JACKSON",
-      "designation": "SALES I",
-      "manager": 2,
-      "hired_on": "01-02-1996",
-      "salary": 65000,
-      "commission": "500virus",
-      "dept": 3,
-      "img": "../images/act10.jpg"
-    },
-    {
-      "id" : 22,
-      "year": 2004,
-      "make": "Acura",
-      "model": "RSX",
-      "body_styles": "[\"Coupe\"]",
-      "name": "BRUCE WILLIS",
-      "designation": "SALES I",
-      "manager": 2,
-      "hired_on": "01-02-1996",
-      "salary": 65000,
-      "commission": "500virus",
-      "dept": 3,
-      "img": "../images/act11.jpg"
-    },
-    {
-      "id" : 23,
-      "year": 2004,
-      "make": "Acura",
-      "model": "TL",
-      "body_styles": "[\"Sedan\"]",
-      "name": "JOHNNY DEPP",
-      "designation": "SALES I",
-      "manager": 2,
-      "hired_on": "01-02-1996",
-      "salary": 65000,
-      "commission": "500virus",
-      "dept": 3,
-      "img": "../images/act11.jpg"
+      name: 'VINCENT CASSEL',
+      id: 12,
+      films: 'OM',
+      img: "../images/act11.jpg",
+      imgThumbnail: "../images/act12Thumbnail.jpg"
     }
+
+
   ]
 
 }
